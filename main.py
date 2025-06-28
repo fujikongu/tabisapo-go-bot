e64790f61fea2bea0e0a1a6884cc59c9dc29bf5e
fujikongu <fujikonbu0507@gmail.com>
2025-06-28 23:09:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, requfrom flask import Flask, request, abort
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
