59c88e6c5cc638ea9ae036567303779cadfadc19
fujikongu <fujikonbu0507@gmail.com>
2025-06-28 21:33:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -122,4 +122,4 @@ def handle_location(event):
 # Render用
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port))
+    app.run(host="0.0.0.0", port=port)
